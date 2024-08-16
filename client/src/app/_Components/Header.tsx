dbaf4eaa0d9587ed9fd4672f9cd4851da94bbf22
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-08-16 19:24:14 +0200

    Adjusted style and some colors

--- client/src/app/_Components/Header.tsx
+++ client/src/app/_Components/Header.tsx
@@ -3,9 +3,10 @@ import Image from 'next/image';
 function Header() {
   return (
     <header className="bg-white">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 shadow-md">
+      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+        {' '}
+        {/*shadow-md*/}
         <Image src="/logo.svg" alt="logo" width={170} height={170} />
-
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Global" className="hidden md:block">
             <ul className="flex items-center gap-6 text-sm">
@@ -74,14 +75,14 @@ function Header() {
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               <a
-                className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-[#009dffa5]"
+                className="block rounded-md bg-blue-700 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-[#6EACDA]"
                 href="#"
               >
                 Login
               </a>
 
               <a
-                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-blue-700 transition hover:text-blue-600/75 sm:block"
                 href="#"
               >
                 Register
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import Hero from './_Components/Hero';
 
 export default function Home() {
-  return <div>Hello Fucking World!</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
