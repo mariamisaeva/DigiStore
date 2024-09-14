88b05e4d2297f07a85ecdd336ef1fad6bd928811
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-14 19:37:45 +0200

    render attrs.

--- client/src/app/product-details/[productid]/_components/ProductInfo.tsx
+++ client/src/app/product-details/[productid]/_components/ProductInfo.tsx
@@ -4,7 +4,11 @@ import { Product } from '../../../_utils/productsAPI';
 function ProductInfo({ product }: { product: Product }) {
   return (
     <div>
-      <h1>ProductInfo</h1>
+      <h2 className={'text-[28px]'}>{product?.attributes?.title}</h2>
+      <h2 className={'text-[15px] text-gray-400'}>
+        {product?.attributes?.category}
+      </h2>
+      <h2>${product?.attributes?.price}</h2>
     </div>
   );
 }
