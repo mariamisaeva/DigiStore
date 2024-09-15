023beab8452f7cdce517708076eef0f51fb28799
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-15 20:16:24 +0200

    Added

--- client/src/app/product-details/[productid]/page.tsx
+++ client/src/app/product-details/[productid]/page.tsx
@@ -58,7 +58,11 @@ function DetailsPage({ params }: any) {
       <SmallNavbar path={path} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 flex-col mt-10 justify-around gap-5 sm:gap:0">
-        {productDetails && <ProductImage product={productDetails} />}
+        {productDetails ? (
+          <ProductImage product={productDetails} />
+        ) : (
+          <h4>Loading ... </h4>
+        )}
         {productDetails && <ProductInfo product={productDetails} />}
       </div>
       {/* <h1>Product Id: {params?.productId}</h1> */}
