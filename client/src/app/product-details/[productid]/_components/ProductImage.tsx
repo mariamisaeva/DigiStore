0c010ff41c942bf379444832937ee18a96d23507
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-15 19:53:53 +0200

    Added

--- client/src/app/product-details/[productid]/_components/ProductImage.tsx
+++ client/src/app/product-details/[productid]/_components/ProductImage.tsx
@@ -4,15 +4,17 @@ import Image from 'next/image';
 function ProductImage({ product }: { product: Product }) {
   return (
     <div>
-      <Image
-        src={product?.attributes?.image?.data?.attributes?.url}
-        alt={product?.attributes?.title}
-        width={400}
-        height={400}
-        className="rounded-3xl "
-      />
-
-      <div className="w-[400px] h-[225px] bg-slate-200 rounded-lg animate-pulse"></div>
+      {product ? (
+        <Image
+          src={product?.attributes?.image?.data?.attributes?.url}
+          alt={product?.attributes?.title}
+          width={400}
+          height={400}
+          className="rounded-3xl "
+        />
+      ) : (
+        <div className="w-[400px] h-[225px] bg-slate-200 rounded-lg animate-pulse"></div>
+      )}
     </div>
   );
 }
