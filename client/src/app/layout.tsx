d7bc01b0f61b14dd46bddcf0c09286dae4a68598
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-18 20:54:09 +0200

    added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      isCookieless={process.env.NEXT_PUBLIC_CLERK_COOKIELESS === 'true'}
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>
           {/* <Header />
