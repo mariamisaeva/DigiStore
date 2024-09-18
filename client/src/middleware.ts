bff01c3dc69973e3ee84b5d0a3d03a501f611a58
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-18 21:18:18 +0200

    added

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -17,8 +17,6 @@ export const config = {
   ],
 };
 
-// export default clerkMiddleware();
-
 // export const config = {
 //   matcher: [
 //     // Skip Next.js internals and all static files, unless found in search params
