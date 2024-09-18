655cd51d028f1b65870744764a4bc234967da08a
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-18 21:18:56 +0200

    added

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -21,4 +21,3 @@ export const config = {
 //     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
 //     // Always run for API routes
 //     '/(api|trpc)(.*)',
-// };
