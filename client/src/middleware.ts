58cbdc293f8fb5dca07e451e520b2f82b8c2773b
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-18 21:09:38 +0200

    added

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
@@ -11,12 +17,15 @@ export const config = {
   ],
 };
 
-// import { authMiddleware } from '@clerk/nextjs';
+// import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// export default authMiddleware({
-//   cookieless: true, // Ensures cookieless session handling
-// });
+// export default clerkMiddleware();
 
 // export const config = {
-//   matcher: ['/((?!api|static|.*\\..*|_next).*)', '/'],
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
 // };
