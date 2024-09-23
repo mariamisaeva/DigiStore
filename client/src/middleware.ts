be2943930cb8a0ebf2298d609379557dba850faf
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-23 21:41:51 +0200

    adjusted

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -10,7 +10,7 @@ export default clerkMiddleware((auth, request) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in
+    // Skip Next.js internals and all static files, unless found
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
