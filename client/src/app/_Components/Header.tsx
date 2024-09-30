f0b62b332fd2f53a2306c28a757d713bc140f0a5
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-30 21:52:31 +0200

    Added

--- client/src/app/_Components/Header.tsx
+++ client/src/app/_Components/Header.tsx
@@ -77,6 +77,7 @@ function Header() {
 
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
+                {!user ?}
                 <a
                   className="block rounded-md bg-blue-700 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-[#6EACDA]"
                   href="#"
@@ -90,7 +91,6 @@ function Header() {
                 >
                   Register
                 </a>
-                {!user ? }
 
               </div>
 
