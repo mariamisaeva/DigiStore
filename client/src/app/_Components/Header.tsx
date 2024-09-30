c6be53665a9820626e40d0ac3ab74ac4886eeefb
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-09-30 21:54:28 +0200

    Added

--- client/src/app/_Components/Header.tsx
+++ client/src/app/_Components/Header.tsx
@@ -76,24 +76,25 @@ function Header() {
             </nav>
 
             <div className="flex items-center gap-4">
-            {!user ? 
-            
-              <div className="sm:flex sm:gap-4">
-                <a
-                  className="block rounded-md bg-blue-700 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-[#6EACDA]"
-                  href="#"
-                >
-                  Login
-                </a>
+              {!user ? (
+                <div className="sm:flex sm:gap-4">
+                  <a
+                    className="block rounded-md bg-blue-700 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-[#6EACDA]"
+                    href="#"
+                  >
+                    Login
+                  </a>
 
-                <a
-                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-blue-700 transition hover:text-blue-600/75 sm:block"
-                  href="#"
-                >
-                  Register
-                </a>
-              </div>
-        }
+                  <a
+                    className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-blue-700 transition hover:text-blue-600/75 sm:block"
+                    href="#"
+                  >
+                    Register
+                  </a>
+                </div>
+              ) : (
+                <div></div>
+              )}
 
               <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
                 <span className="sr-only">Toggle menu</span>
