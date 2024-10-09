43dfc183c92825880e0a28dff1fbc8765421d2e9
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-09 13:29:41 +0200

    added style

--- client/src/app/_Components/Cart.tsx
+++ client/src/app/_Components/Cart.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Cart() {
   return (
     <div
-      className="h-[300px] w-[250px] bg-gray-200 z-10 rounded-md border shadow-sm absolute mx-10 right-10"
+      className="h-[300px] w-[250px] bg-gray-200 z-10 rounded-md border shadow-sm absolute mx-10 right-10 top-12 p-5 overflow-auto"
       style={{ color: 'black' }}
     >
       Cart
