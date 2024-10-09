fab9b7b6f323959ba7e49387c9d27788ffb38771
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-09 15:26:21 +0200

    dynamic

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -46,10 +46,8 @@ function CartPage() {
                     </div>
 
                     <div className="flex flex-1 items-center justify-end gap-2">
-                      <div>
-                        <dt className="inline">Price: $</dt>
-                        {item?.product?.attributes?.price}
-                        <dd className="inline"></dd>
+                      <div style={{ color: 'black' }}>
+                        ${item?.product?.attributes?.price}
                       </div>
 
                       <button className="text-gray-600 transition hover:text-red-600">
@@ -199,24 +197,9 @@ function CartPage() {
               <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
                 <div className="w-screen max-w-lg space-y-4">
                   <dl className="space-y-0.5 text-sm text-gray-700">
-                    <div className="flex justify-between">
-                      <dt>Subtotal</dt>
-                      <dd>£250</dd>
-                    </div>
-
-                    <div className="flex justify-between">
-                      <dt>VAT</dt>
-                      <dd>£25</dd>
-                    </div>
-
-                    <div className="flex justify-between">
-                      <dt>Discount</dt>
-                      <dd>-£20</dd>
-                    </div>
-
                     <div className="flex justify-between !text-base font-medium">
                       <dt>Total</dt>
-                      <dd>£200</dd>
+                      <dd>{getTotal()}</dd>
                     </div>
                   </dl>
 
