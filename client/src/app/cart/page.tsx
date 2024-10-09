6c413d9c690e745f9d92bc85fe23a2dcf30017fc
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-09 15:09:01 +0200

    dynamic data- cart page

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -32,37 +32,25 @@ function CartPage() {
 
                     <div>
                       <h3 className="text-sm text-gray-900">
-                        Basic Tee 6-Pack
+                        {item?.product?.attributes?.title}
                       </h3>
 
                       <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
                         <div>
-                          <dt className="inline">Size:</dt>
-                          <dd className="inline">XXS</dd>
-                        </div>
-
-                        <div>
-                          <dt className="inline">Color:</dt>
-                          <dd className="inline">White</dd>
+                          <dt className="inline">Category:</dt>
+                          <dd className="inline">
+                            {item?.product?.attributes?.category}
+                          </dd>
                         </div>
                       </dl>
                     </div>
 
                     <div className="flex flex-1 items-center justify-end gap-2">
-                      <form>
-                        <label htmlFor="Line1Qty" className="sr-only">
-                          {' '}
-                          Quantity{' '}
-                        </label>
-
-                        <input
-                          type="number"
-                          min="1"
-                          defaultValue="1"
-                          id="Line1Qty"
-                          className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                        />
-                      </form>
+                      <div>
+                        <dt className="inline">Price: $</dt>
+                        {item?.product?.attributes?.price}
+                        <dd className="inline"></dd>
+                      </div>
 
                       <button className="text-gray-600 transition hover:text-red-600">
                         <span className="sr-only">Remove item</span>
