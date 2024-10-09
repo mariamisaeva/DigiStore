50e092308f8e39de4bda9f15dcec98386d7e1859
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-09 14:00:36 +0200

    cart comp.

--- client/src/app/_Components/Cart.tsx
+++ client/src/app/_Components/Cart.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useCart } from '../_context/cartContext';
+import Image from 'next/image';
 
 function Cart() {
+  const { cart, setCart } = useCart();
   return (
     <div
       className="h-[300px] w-[250px] bg-gray-200 z-10 rounded-md border shadow-sm absolute mx-10 right-10 top-12 p-5 overflow-auto"
@@ -26,29 +29,34 @@ function Cart() {
       </button>
       <div className="mt-4 space-y-6">
         <ul className="space-y-4">
-          <li className="flex items-center gap-4">
-            <img
-              src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-              alt=""
-              className="size-16 rounded object-cover"
-            />
+          {cart?.map((item) => (
+            <li key={item?.id} className="flex items-center gap-4">
+              <Image
+                src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
+                alt="Product Image"
+                width={80} // You can adjust the width and height according to your design
+                height={80}
+                className="size-16 rounded object-cover"
+              />
 
-            <div>
-              <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
+              <div>
+                <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
 
-              <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                <div>
-                  <dt className="inline">Size:</dt>
-                  <dd className="inline">XXS</dd>
-                </div>
+                <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
+                  <div>
+                    <dt className="inline">Size:</dt>
+                    <dd className="inline">XXS</dd>
+                  </div>
 
-                <div>
-                  <dt className="inline">Color:</dt>
-                  <dd className="inline">White</dd>
-                </div>
-              </dl>
-            </div>
-          </li>
+                  <div>
+                    <dt className="inline">Color:</dt>
+                    <dd className="inline">White</dd>
+                  </div>
+                </dl>
+              </div>
+            </li>
+          ))}
+
           {/* 
           <li className="flex items-center gap-4">
             <img
