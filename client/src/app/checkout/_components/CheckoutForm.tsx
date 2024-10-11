eb1aec2d0fd29c3a91e893c150256fb37b241d2a
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-11 16:48:26 +0200

    stripe integrating

--- client/src/app/checkout/_components/CheckoutForm.tsx
+++ client/src/app/checkout/_components/CheckoutForm.tsx
@@ -3,8 +3,12 @@ import { PaymentElement } from '@stripe/react-stripe-js';
 const CheckoutForm = () => {
   return (
     <form>
-      <PaymentElement />
-      <button>Submit</button>
+      <div className="mx-32 md:mx[300px] mt-20">
+        <PaymentElement />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
     </form>
   );
 };
