8488637f070c38546dfee0137ee842c2ae633b09
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-11 17:56:17 +0200

    stripe intent

--- client/src/app/api/route.tsx
+++ client/src/app/api/route.tsx
@@ -1,24 +1,26 @@
 import { NextResponse, NextRequest } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  typescript: true,
-  apiVersion: '2022-11-15',
-});
+const stripe = new (Stripe as any)(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: NextRequest) {
-  const { data } = await req.json();
+  const data = await req.json();
   const { amount } = data;
+
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: Number(amount) * 100,
+      amount: Number(amount),
       currency: 'USD',
+      automatic_payment_methods: {
+        enabled: true,
+      },
     });
 
-    return new NextResponse(paymentIntent.client_secret, { status: 200 });
+    return NextResponse.json(
+      { client_secret: paymentIntent.client_secret },
+      { status: 200 },
+    );
   } catch (error: any) {
-    return new NextResponse(error, {
-      status: 400,
-    });
+    return NextResponse.json({ message: error.message }, { status: 400 });
   }
 }
