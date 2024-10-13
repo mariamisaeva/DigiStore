11f07b3a514cb8ae2336521026f721df0a91b93e
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-13 18:57:39 +0200

    trying

--- server/config/server.js
+++ server/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
+    host: env('HOST', process.env.HOST || '0.0.0.0'),
     port: env.int('PORT', process.env.PORT || 1337),
     app: {
         keys: env.array('APP_KEYS'),
