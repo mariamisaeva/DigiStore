cdf563c58c8b755944d322b68ef31fb070c399b4
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-15 18:06:55 +0200

    deployed

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -7,12 +7,20 @@ module.exports = [
             contentSecurityPolicy: {
                 useDefaults: true,
                 directives: {
-                    'connect-src': ["'self'", 'https:'],
+                    'connect-src': ["'self'", 'https:', process.env.FRONTEND_URL], //tp link with the frontend
                     'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
                     'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
                     upgradeInsecureRequests: null,
                 },
             },
+        },
+    },
+    {
+        name: 'strapi::cors',
+        config: {
+            enabled: true,
+            origin: [process.env.FRONTEND_URL], // Allow your Vercel frontend to make requests
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
         },
     },
     'strapi::cors',
