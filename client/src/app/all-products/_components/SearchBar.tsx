dd6c59fda5df6307de4c002b69c0bacce785a80a
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-18 16:50:39 +0200

    searchBar

---
+++ client/src/app/all-products/_components/SearchBar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface SearchBarProps {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+  selectedCategory: string;
+  setSelectedCategory: (category: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({
+  searchQuery,
+  setSearchQuery,
+  selectedCategory,
+  setSelectedCategory,
+}) => {
+  return (
+    <div className="mb-6">
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search Products..."
+        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+      />
+
+      <select
+        title="Search By Category"
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+        className="p-2 border border-gray-300 rounded-md w-full"
+      ></select>
+    </div>
+  );
+};
+
+export default SearchBar;
