8a896d735025f385f8b75d7a2153019cda81cb26
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-18 19:00:16 +0200

    corrected hero

--- client/src/app/_Components/Hero.tsx
+++ client/src/app/_Components/Hero.tsx
@@ -12,7 +12,7 @@ function Hero() {
       <section className="relative bg-[url(https://res.cloudinary.com/dm4vls99s/image/upload/v1728992758/DigitalStore-Project/DALL_E_2024-10-15_13.14.37_-_A_light_blue_background_for_a_digital_products_website_designed_to_be_versatile_for_items_like_courses_e-books_and_software._The_background_feature_xumxd4.webp)] bg-cover bg-center bg-no-repeat">
         <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
-        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-1">
           <div className="max-w-xl text-left ltr:sm:text-left rtl:sm:text-right">
             <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
               Unlock Your Potential
