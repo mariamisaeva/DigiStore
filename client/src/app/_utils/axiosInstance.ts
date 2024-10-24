1d88a4dc1b0951688ba5b7f23789051f1c9ec0ee
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-10-24 19:57:13 +0200

    fixed base_url

--- client/src/app/_utils/axiosInstance.ts
+++ client/src/app/_utils/axiosInstance.ts
@@ -3,7 +3,8 @@ import axios, { AxiosInstance } from 'axios';
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
 // const cleanAPIkey = apiKey?.replace('Bearer', '');
 
-const apiURL = 'http://localhost:1337/api';
+const apiURL = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+// 'http://localhost:1337/api';
 //  process.env.NEXT_PUBLIC_STRAPI_API_URL
 const axiosClient: AxiosInstance = axios.create({
   baseURL: apiURL,
