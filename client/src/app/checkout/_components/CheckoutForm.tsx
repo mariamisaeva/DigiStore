8ddd83dde906f792d173e0073e122843662ce682
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-11-03 17:50:22 +0100

    ENV fixed for production

--- client/src/app/checkout/_components/CheckoutForm.tsx
+++ client/src/app/checkout/_components/CheckoutForm.tsx
@@ -68,11 +68,16 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({
 
     const { client_secret: clientSecret } = await res.json();
 
+    const successUrl =
+      process.env.NODE_ENV === 'production'
+        ? `${process.env.LIVE_URL}/payment-success`
+        : 'http://localhost:3000/payment-success';
+
     const result = await stripe.confirmPayment({
       clientSecret,
       elements,
       confirmParams: {
-        return_url: 'http://localhost:3000/payment-success',
+        return_url: successUrl, //'http://localhost:3000/payment-success',
       },
     });
 
