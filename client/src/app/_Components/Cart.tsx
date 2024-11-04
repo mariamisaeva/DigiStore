6d650c56cc8f8f98ecfc7787e4dd220cc8871be7
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-11-04 18:22:50 +0100

    rm comments

--- client/src/app/_Components/Cart.tsx
+++ client/src/app/_Components/Cart.tsx
@@ -63,55 +63,6 @@ function Cart() {
               </div>
             </li>
           ))}
-
-          {/* 
-          <li className="flex items-center gap-4">
-            <img
-              src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-              alt=""
-              className="size-16 rounded object-cover"
-            />
-
-            <div>
-              <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
-
-              <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                <div>
-                  <dt className="inline">Size:</dt>
-                  <dd className="inline">XXS</dd>
-                </div>
-
-                <div>
-                  <dt className="inline">Color:</dt>
-                  <dd className="inline">White</dd>
-                </div>
-              </dl>
-            </div>
-          </li> */}
-
-          {/* <li className="flex items-center gap-4">
-            <img
-              src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-              alt=""
-              className="size-16 rounded object-cover"
-            />
-
-            <div>
-              <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
-
-              <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                <div>
-                  <dt className="inline">Size:</dt>
-                  <dd className="inline">XXS</dd>
-                </div>
-
-                <div>
-                  <dt className="inline">Color:</dt>
-                  <dd className="inline">White</dd>
-                </div>
-              </dl>
-            </div>
-          </li> */}
         </ul>
 
         <div className="space-y-4 text-center">
@@ -121,13 +72,6 @@ function Cart() {
           >
             View my cart ({cart?.length})
           </Link>
-
-          {/* <a
-            href="#"
-            className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
-          >
-            Checkout
-          </a> */}
 
           <Link
             href="/all-products"
