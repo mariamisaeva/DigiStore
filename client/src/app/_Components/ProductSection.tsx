5b48d8ac558c6b9bf5540792e337300733f338bd
Mariam Hammad <142991068+mariamisaeva@users.noreply.github.com>
2025-06-18 13:04:23 +0200

    refactor: fetch products server-side

--- client/src/app/_Components/ProductSection.tsx
+++ client/src/app/_Components/ProductSection.tsx
@@ -1,42 +1,17 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import ProductList from './ProductList';
-import { getLatestProducts, StrapiResponse } from '../_utils/productsAPI';
+import React from 'react';
 import { AxiosResponse } from 'axios';
-import { Product } from '../_utils/productsAPI';
+import ProductList from './ProductList';
+import { getLatestProducts, Product, StrapiResponse } from '../_utils/productsAPI';
 
-export default function ProductSection() {
-  const [productList, setProductList] = useState<Product[]>([]);
-
-  //a state for featured products
-  const [featuredProducts, setFeaturedProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetchLatestProducts();
-  }, []);
-
-  const fetchLatestProducts = async () => {
-    try {
-      const res: AxiosResponse<StrapiResponse<Product[]>> =
-        await getLatestProducts();
-      //   console.log(res.data.data);
-      //   setProductList(res.data.data);
-      const allProducts = res.data.data; //get all products
-      //select 8 random
-      const randomProducts = [...allProducts]
-        .sort(() => Math.random() - 0.5) //shuffle
-        .slice(0, 4);
-
-      setFeaturedProducts(randomProducts);
-      setProductList(allProducts); //store all products
-    } catch (err) {
-      console.error(err);
-    }
-  };
+export default async function ProductSection() {
+  const res: AxiosResponse<StrapiResponse<Product[]>> = await getLatestProducts();
+  const allProducts = res.data.data;
+  const featuredProducts = [...allProducts]
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 4);
 
   return (
     <div className="px-6 sm:px-10 md:px-20 py-10">
-      {/*Featured products section*/}
       <h1
         id="featured-products"
         className="text-2xl md:text-3xl font-bold text-black my-4"
@@ -44,10 +19,6 @@ export default function ProductSection() {
         Featured Products
       </h1>
       <ProductList productList={featuredProducts} />
-
-      {/*All products section*/}
-      {/* <h1 className="text-3xl font-bold text-black my-4">All Products</h1>
-      <ProductList productList={productList} /> */}
     </div>
   );
 }
--- client/src/app/product-details/[productid]/page.tsx
+++ client/src/app/product-details/[productid]/page.tsx
@@ -1,5 +1,4 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   getProductById,
   Product,
@@ -11,89 +10,36 @@ import ProductImage from './_components/ProductImage';
 import ProductInfo from './_components/ProductInfo';
 import { AxiosResponse } from 'axios';
 import ProductList from '../../_Components/ProductList';
-import { usePathname } from 'next/navigation';
-import SkeletonProductInfo from './_components/SkeletonProductInfo';
 
 type paramsType = {
   productid: string;
 };
 
-function DetailsPage({ params }: { params: paramsType }) {
-  const path: any = usePathname();
-  const [productDetails, setProductDetails] = useState<Product | null>(null);
-
-  const [similarProductList, setSimilarProductList] = useState<
-    Product[] | null
-  >(null);
-
-  //   console.log('Params: ', params);
-  //   let productId = params?.productId;
-  //   productId = Number(productId);
+export default async function DetailsPage({ params }: { params: paramsType }) {
   const productId = Number(params?.productid);
+  const productRes: AxiosResponse<StrapiResponse<Product>> = await getProductById(productId);
+  const productDetails = productRes.data.data;
 
-  useEffect(() => {
-    async function fetchProductById() {
-      //   console.log('PRODUCT_ID: ', productId);
-      //   console.log('PARAMS.PRODUCTID: ', params.productid);
-      //   console.log('PARAMS: ', params);
+  const categoryRes: AxiosResponse<StrapiResponse<Product[]>> = await getProductByCategory(
+    productDetails.attributes.category,
+  );
+  const similarProductList = categoryRes.data.data;
 
-      if (!productId) {
-        console.error('Product ID is undefined!');
-        return;
-      }
-
-      try {
-        const res = await getProductById(productId);
-        // console.log(res.data.data);
-        setProductDetails(res.data.data);
-        // getProductByCategory(res.data.data); //func call
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    fetchProductById();
-  }, [productId]);
-
-  useEffect(() => {
-    if (productDetails) {
-      fetchProductByCategory(productDetails);
-    }
-  }, [productDetails]);
-
-  async function fetchProductByCategory(product: Product) {
-    try {
-      const res: AxiosResponse<StrapiResponse<Product[]>> =
-        await getProductByCategory(product?.attributes?.category);
-      //   console.log('Here is the Response: ', res?.data?.data);
-      setSimilarProductList(res?.data?.data);
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  const path = `/product-details/${productId}`;
 
   return (
     <div className="text-black px-10 py-8 md:px-28">
       <SmallNavbar path={path} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 flex-col mt-10 justify-around gap-5 sm:gap:0">
-        {productDetails ? (
-          <ProductImage product={productDetails} />
-        ) : (
-          <SkeletonProductInfo />
-        )}
-        {productDetails && <ProductInfo product={productDetails} />}
+        <ProductImage product={productDetails} />
+        <ProductInfo product={productDetails} />
       </div>
-      {/* <h1>Product Id: {params?.productId}</h1> */}
 
       <div>
-        <h2 className="text-xl font-bold text-center mt-24 mb-5">
-          Similar Products
-        </h2>
+        <h2 className="text-xl font-bold text-center mt-24 mb-5">Similar Products</h2>
         <ProductList productList={similarProductList || []} />
       </div>
     </div>
   );
 }
-
-export default DetailsPage;
