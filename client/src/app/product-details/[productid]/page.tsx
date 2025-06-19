883fd02ccb8bd5285ec43682ec8838fc7fbc0c9f
Mariam Hammad <142991068+mariamisaeva@users.noreply.github.com>
2025-06-19 08:49:03 +0200

    Improve product details loading

--- client/src/app/product-details/[productid]/page.tsx
+++ client/src/app/product-details/[productid]/page.tsx
@@ -10,6 +10,7 @@ import ProductImage from './_components/ProductImage';
 import ProductInfo from './_components/ProductInfo';
 import { AxiosResponse } from 'axios';
 import ProductList from '../../_Components/ProductList';
+import { notFound } from 'next/navigation';
 
 type paramsType = {
   productid: string;
@@ -17,29 +18,39 @@ type paramsType = {
 
 export default async function DetailsPage({ params }: { params: paramsType }) {
   const productId = Number(params?.productid);
-  const productRes: AxiosResponse<StrapiResponse<Product>> = await getProductById(productId);
-  const productDetails = productRes.data.data;
 
-  const categoryRes: AxiosResponse<StrapiResponse<Product[]>> = await getProductByCategory(
-    productDetails.attributes.category,
-  );
-  const similarProductList = categoryRes.data.data;
+  try {
+    const productRes: AxiosResponse<StrapiResponse<Product>> = await getProductById(productId);
+    const productDetails = productRes.data.data;
 
-  const path = `/product-details/${productId}`;
+    if (!productDetails) {
+      notFound();
+    }
 
-  return (
-    <div className="text-black px-10 py-8 md:px-28">
-      <SmallNavbar path={path} />
+    const categoryRes: AxiosResponse<StrapiResponse<Product[]>> = await getProductByCategory(
+      productDetails.attributes.category,
+    );
+    const similarProductList = categoryRes.data.data;
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 flex-col mt-10 justify-around gap-5 sm:gap:0">
-        <ProductImage product={productDetails} />
-        <ProductInfo product={productDetails} />
+    const path = `/product-details/${productId}`;
+
+    return (
+      <div className="text-black px-10 py-8 md:px-28">
+        <SmallNavbar path={path} />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 flex-col mt-10 justify-around gap-5 sm:gap:0">
+          <ProductImage product={productDetails} />
+          <ProductInfo product={productDetails} />
+        </div>
+
+        <div>
+          <h2 className="text-xl font-bold text-center mt-24 mb-5">Similar Products</h2>
+          <ProductList productList={similarProductList || []} />
+        </div>
       </div>
-
-      <div>
-        <h2 className="text-xl font-bold text-center mt-24 mb-5">Similar Products</h2>
-        <ProductList productList={similarProductList || []} />
-      </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error('Error loading product details', error);
+    notFound();
+  }
 }
